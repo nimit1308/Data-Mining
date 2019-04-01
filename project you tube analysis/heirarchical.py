af725e1ccf4341053fbdf543c17a9fce442969de
Nimit Agrawal <nimitagrawal1308@gmail.com>
2019-04-01 23:45:06 +0530

    you tube Indian data analysis

---
+++ project you tube analysis/heirarchical.py
@@ -0,0 +1,75 @@
+import numpy as np
+import pandas as pd
+# Importing the dataset
+data = pd.read_csv('xclara.csv')
+
+data.head()
+# Getting the values and plotting it
+f1 = data['V1'].values
+f2 = data['V2'].values
+X = np.array(list(zip(f1, f2)))
+import matplotlib.pyplot as plt
+
+labels = range(1, len(X)-1)  
+plt.figure(figsize=(10, 7))  
+plt.subplots_adjust(bottom=0.1)  
+plt.scatter(X[:,0],X[:,1], label='True Position')
+
+for label, x, y in zip(labels, X[:, 0], X[:, 1]):  
+    plt.annotate(
+        label,
+        xy=(x, y), xytext=(-3, 3),
+        textcoords='offset points', ha='right', va='bottom')
+plt.show()  
+from scipy.cluster.hierarchy import dendrogram, linkage  
+from matplotlib import pyplot as plt
+
+linked = linkage(X, 'complete')
+
+labelList = range(1, len(X)+1)
+
+plt.figure(figsize=(10, 7))  
+dendrogram(linked,  
+            orientation='top',
+            labels=labelList,
+            distance_sort='descending',
+            show_leaf_counts=True)
+plt.show()  
+
+def fancy_dendrogram(*args, **kwargs):
+    max_d = kwargs.pop('max_d', None)
+    if max_d and 'color_threshold' not in kwargs:
+        kwargs['color_threshold'] = max_d
+    annotate_above = kwargs.pop('annotate_above', 0)
+
+    ddata = dendrogram(*args, **kwargs)
+
+    if not kwargs.get('no_plot', False):
+        plt.title('Hierarchical Clustering Dendrogram (truncated)')
+        plt.xlabel('sample index or (cluster size)')
+        plt.ylabel('distance')
+        for i, d, c in zip(ddata['icoord'], ddata['dcoord'], ddata['color_list']):
+            x = 0.5 * sum(i[1:3])
+            y = d[1]
+            if y > annotate_above:
+                plt.plot(x, y, 'o', c=c)
+                plt.annotate("%.3g" % y, (x, y), xytext=(0, -5),
+                             textcoords='offset points',
+                             va='top', ha='center')
+        if max_d:
+            plt.axhline(y=max_d, c='k')
+    return ddata
+
+fancy_dendrogram(
+    linked,
+    truncate_mode='lastp',
+    p=12,
+    leaf_rotation=90.,
+    leaf_font_size=12.,
+    show_contracted=True,
+    annotate_above=10,  # useful in small plots so annotations don't overlap
+)
+plt.show()
+
+
+
